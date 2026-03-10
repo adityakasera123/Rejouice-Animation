c1aa40364247dea6bab47971d78f39308b0f2667
Aditya <adityakasera40@gmail.com>
2026-03-10 23:18:01 +0530

    landing page complete

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function cursorEffect(){
+    let cursor = document.querySelector("#cursor");
+let page1Content = document.querySelector("#page1-content");
 
+page1Content.addEventListener("mousemove", function(dets){
+gsap.to(cursor,{
+    left: dets.x,
+    top: dets.y,
+ 
+})
+})
+page1Content.addEventListener("mouseenter",function(){
+gsap.to(cursor,{
+    scale:1
+})
+})
+page1Content.addEventListener("mouseleave",function(){
+gsap.to(cursor,{
+    scale:0
+})
+})
+
+}
+cursorEffect();
