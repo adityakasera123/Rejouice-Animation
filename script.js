d7cb3ebb8b0d163de62145619784566cec4f85ed
Aditya <adityakasera40@gmail.com>
2026-03-19 22:40:34 +0530

    page5 done with Loader Add in front

--- script.js
+++ script.js
@@ -123,19 +123,52 @@ string.addEventListener("mouseleave", function () {
 }
 stringAnimation();
 
-function SwipperJs(){
+function slidesAnimation(){
+  function SwipperJs(){
  var swiper = new Swiper(".mySwiper", {
       slidesPerView: 1,
       spaceBetween: 30,
       loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
       },
     });
   }
   SwipperJs();
+}
+slidesAnimation();
+
+
+function loaderAnimation(){
+  let tl=gsap.timeline()
+
+tl.from("#loader h3",{
+  x:40,
+  opacity:0,
+  duration:1,
+  stagger:0.1,
+})
+tl.to("#loader h3",{
+  x:-10,
+  opacity:0,
+  duration:1,
+  stagger:0.1,
+})
+
+tl.to("#loader",{
+  opacity:0,
+})
+tl.from("#page1-content h1 span",{
+  y:100,
+  opacity:0,
+  stagger:0.1,
+  duration:0.5,
+  delay:-0.5
+})
+tl.to("#loader",{
+  display:"none",
+})
+
+}
+loaderAnimation();
